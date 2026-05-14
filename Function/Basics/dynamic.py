8405224925485650f09cf8bb3f6d27327c8bb312
Ankit Poudel <pdlankit2000@gmail.com>
2026-05-14 19:01:17 +0545

    Learnt json with python and also dynamic functions

---
+++ Function/Basics/dynamic.py
@@ -0,0 +1,47 @@
+
+#! args and kwargs
+
+# from rich import print
+# def addSum(*args):
+#     total = 0
+#     for i in range(len(args)):
+#         total += args[i]
+#     return total
+
+# # numbers = [1,2,3,4,5,6,7]
+# totals = addSum(1,2,3,4,5,6,7)
+# print(totals
+
+
+# numbers = [1,2,3,4,5]
+# def addSum(numbers):
+#     total = 0
+#     for i in numbers:
+#        total+= i
+#     return total
+# print(addSum(numbers))
+
+# from rich import print
+# def printName(**temp):
+#     print(f"Hello  +[bold ]  {temp['lname']} [/bold magenta]")
+
+# printName(fname = "Anamika",lname = "Sony")
+
+
+# from rich import print
+
+# print(
+#     "[bold red]Hey Anamika[/bold red]"
+#     )
+
+# from rich import print
+# addSum = lambda a,b:a+b
+# print(addSum(3,4))
+
+# def factorial(n):
+#     if n ==0 or n ==1:
+#         return 1
+#     else:
+#         return n * factorial(n-1)
+    
+# print(factorial(5))    
---
+++ Json_Python/basic.py
@@ -0,0 +1,25 @@
+# import json
+# data = {
+#     "name":"Ankit",
+#     "age":21
+
+# }
+
+# print(type({
+#     "name":"Ankit",
+#     "age":21
+    
+# }))
+
+
+# json_data = json.dumps(data)# this is converted to a json format
+# print(json_data)
+# print(type(json_data))
+
+# python_data = json.loads(json_data) # this is converted to a python format
+# print(type(python_data))
+import json
+with open("streak.json","r") as file:
+       data = json.load(file)
+
+print(data.items())    
